e3ee1d5e618e358578703887c5383169b0868c73
Conway <yawnoc@pm.me>
2021-08-02 18:33:55 +0800

    Create defence.cmd and defence.html

---
+++ defence.cmd
@@ -0,0 +1,47 @@
+%%
+%title
+  Ph.D. Defence
+%resources
+  <script src="paged.polyfill.js"></script>
+%css a~~~~~~~~
+  :root {
+    --solid-line: 1px solid black;
+  }
+  @page {
+    border-top: var(--solid-line);
+    margin: 2cm;
+    size: A4;
+    @top-left {
+      content: "Conway Li";
+      margin-bottom: 0.5em;
+      vertical-align: bottom;
+    }
+    @bottom-right {
+      content: "Page " counter(page) " of " counter(pages);
+      margin-top: 0.5em;
+      vertical-align: top;
+    }
+  }
+  a {
+    color: blue;
+    text-decoration: none;
+  }
+  body {
+    font-family: Tinos, serif;
+    font-size: 11pt;
+  }
+  h1 {
+    font-size: 1.4em;
+    text-align: center;
+  }
+  h2 {
+    font-size: 1.2em;
+  }
+  h3 {
+    font-size: 1.1em;
+  }
+~~~~~~~~
+%%
+
+
+# %title #
